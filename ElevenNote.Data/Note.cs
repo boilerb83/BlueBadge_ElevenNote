7683f9528bd29c25e5ce49475dcc30aedf35695a
boilerb83 <boilerb83@gmail.com>
2020-10-15 13:35:27 -0400

    section 3.02

---
+++ ElevenNote.Data/Note.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Data
+{
+    public class Note
+    {
+        [Key]
+        public int NoteId { get; set; }
+        [Display(Name = "Enter NoteID")]
+
+        [Required]
+        public Guid OwnerId { get; set; }
+        [Display(Name = "Enter Guid")]
+
+        [Required]
+        public string Title { get; set; }
+        [Display(Name = "Enter Title")]
+
+        [Required]
+        public string Content { get; set; }
+        [Display(Name = "Enter Content")]
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+
+    }
+}
